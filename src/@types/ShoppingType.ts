d80dd41fbb766f86766a4a6d32544b9d2534cd0b
madson <madsonsilva2005@gmail.com>
2023-05-03 10:03:48 -0300

    adicionado repository do shopping  e criado a tipagem do shopping

---
+++ src/@types/ShoppingType.ts
@@ -0,0 +1,7 @@
+export interface IShopping {
+  amount: number;
+  address: string;
+  payed_at: Date;
+  product_id: string;
+  customer_id: string;
+}
---
+++ src/repositories/shopping.repository.ts
@@ -0,0 +1,46 @@
+import { IShopping } from "../@types/ShoppingType";
+import prismaClient from "../database/prismaClient";
+
+class shoppingRepository {
+  static async getShoppings() {
+    const shoppings = await prismaClient.shopping.findMany();
+    return shoppings;
+  }
+
+  static async getShopping(userId: string) {
+    const shopping = await prismaClient.shopping.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return shopping;
+  }
+
+  static async createShopping(shopping: IShopping) {
+    const shoppingCreated = await prismaClient.shopping.create({
+      data: {
+        ...shopping,
+      },
+    });
+    return shoppingCreated;
+  }
+
+  static async updateShopping(shoppingId: string, shopping: IShopping) {
+    const shoppingUpdated = await prismaClient.shopping.update({
+      where: { id: shoppingId },
+      data: {
+        ...shopping,
+      },
+    });
+
+    return shoppingUpdated;
+  }
+
+  static async deleteShopping(shoppingId: string) {
+    const shoppingDeleted = await prismaClient.shopping.delete({
+      where: { id: shoppingId },
+    });
+  }
+}
+
+export default shoppingRepository;
