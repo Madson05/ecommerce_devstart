b6bb8357680af394b59116449456242e3416ab1d
madson <madsonsilva2005@gmail.com>
2023-05-07 16:00:42 -0300

    corrigido erro no middleware de autenticação

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 
 import jwt from "jsonwebtoken";
-import { ICustomRequest } from "../@types/CustomRequest";
+
 
 type jwtPayload = {
   id: string;
 };
 
 export const authMiddleware = async (
-  req: ICustomRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
