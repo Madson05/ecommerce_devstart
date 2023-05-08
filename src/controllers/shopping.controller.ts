f4d4c1d2848de64a9e6c4f8bf9a037ec669482b5
madson <madsonsilva2005@gmail.com>
2023-05-07 23:09:42 -0300

    impelementado controle de erros no shoppping através de try catch no controller

--- src/controllers/shopping.controller.ts
+++ src/controllers/shopping.controller.ts
@@ -1,40 +1,70 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import shoppingService from "../services/shopping.service";
-import {IShopping} from "../@types/ShoppingType"
+import { IShopping } from "../@types/ShoppingType";
 
 class shoppingController {
-  static async getShoppings(req: Request, res: Response) {
-    const customerId = req.customerId
-    res.json(await shoppingService.getShoppings(customerId));
+  static async getShoppings(req: Request, res: Response, next: NextFunction) {
+    try {
+      const customerId = req.customerId;
+      res.json(await shoppingService.getShoppings(customerId));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async getShopping(req: Request, res: Response) {
-    const customerId = req.customerId;
-    const {shoppingId} = req.params;
-    res.json(await shoppingService.getShopping(shoppingId, customerId));
+  static async getShopping(req: Request, res: Response, next: NextFunction) {
+    try {
+      const customerId = req.customerId;
+      const { shoppingId } = req.params;
+      res.json(await shoppingService.getShopping(shoppingId, customerId));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async createShopping(req: Request, res: Response) {
-    const { amount, address, payed_at, product_id } = req.body;
-    const customer_id = req.customerId;
-    customer_id
-    const shopping: IShopping = {amount, address, payed_at, product_id, customer_id}
-    res.json(await shoppingService.createshopping(shopping));
+  static async createShopping(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { amount, address, payed_at, product_id } = req.body;
+      const customer_id = req.customerId;
+      const shopping: IShopping = {
+        amount,
+        address,
+        payed_at,
+        product_id,
+        customer_id,
+      };
+      res.json(await shoppingService.createshopping(shopping));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  
-  static async updateShopping(req: Request, res: Response){
-    const { id, amount, address, payed_at, product_id } = req.body;
-    const customer_id = req.customerId;
-    const shopping: IShopping = {amount, address, payed_at, product_id, customer_id}
+  static async updateShopping(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id, amount, address, payed_at, product_id } = req.body;
+      const customer_id = req.customerId;
+      const shopping: IShopping = {
+        amount,
+        address,
+        payed_at,
+        product_id,
+        customer_id,
+      };
 
-    res.json(await shoppingService.updateShopping(id, shopping));
+      res.json(await shoppingService.updateShopping(id, shopping));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async deleteShopping(req: Request, res: Response){
-    const customerId = req.customerId;
-    const { shoppingId } = req.params;
-    res.json(await shoppingService.deleteShopping(shoppingId, customerId));
+  static async deleteShopping(req: Request, res: Response, next: NextFunction) {
+    try {
+      const customerId = req.customerId;
+      const { shoppingId } = req.params;
+      res.json(await shoppingService.deleteShopping(shoppingId, customerId));
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
