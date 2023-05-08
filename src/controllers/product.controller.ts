3ffc9aa8576f43004196ae837a95036b5d7d3d69
madson <madsonsilva2005@gmail.com>
2023-05-07 22:57:56 -0300

    impelementado controle de erros no product através de try catch no controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,35 +1,55 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import productService from "../services/product.service";
-import {IProduct} from "../@types/ProductType"
+import { IProduct } from "../@types/ProductType";
 
 class productController {
-  static async getProducts(req: Request, res: Response) {
-    res.json(await productService.getProducts());
+  static async getProducts(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.json(await productService.getProducts());
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async getProduct(req: Request, res: Response) {
-    const {productId} = req.params;
-    res.json(await productService.getProduct(productId));
+  static async getProduct(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { productId } = req.params;
+      res.json(await productService.getProduct(productId));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async createProduct(req: Request, res: Response) {
-    const { title, description, price } = req.body;
+  static async createProduct(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { title, description, price } = req.body;
 
-    const product: IProduct = {title, description, price}
+      const product: IProduct = { title, description, price };
 
-    res.json(await productService.createproduct(product));
+      res.json(await productService.createproduct(product));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async updateProduct(req: Request, res: Response){
-    const { id, title, description, price } = req.body;
-    const product: IProduct = {title, description, price}
+  static async updateProduct(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id, title, description, price } = req.body;
+      const product: IProduct = { title, description, price };
 
-    res.json(await productService.updateProduct(id, product));
+      res.json(await productService.updateProduct(id, product));
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async deleteProduct(req: Request, res: Response){
-    const { productId } = req.params;
-    res.json(await productService.deleteProduct(productId));
+  static async deleteProduct(req: Request, res: Response, next: NextFunction) {
+    try{
+      const { productId } = req.params;
+      res.json(await productService.deleteProduct(productId));
+    }catch(error){
+      next(error);
+    }
   }
 }
 
