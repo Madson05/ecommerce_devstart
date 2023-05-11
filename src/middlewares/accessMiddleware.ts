cf663b458f6e7fb3fe05ebc342cf3c5315dc8f28
madson <madsonsilva2005@gmail.com>
2023-05-11 11:03:40 -0300

    criado middlware para controle de acesso

---
+++ src/middlewares/accessMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import customerRepository from "../repositories/customer.repository";
+
+export const accessMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const {customerId} = req;
+
+  const customer = await customerRepository.getCustomer(customerId);
+
+  if(!customer){
+    throw new Error("Não autorizado");
+  }
+  
+  if(!customer.canSell){
+    throw new Error("Não autorizado");
+  }
+  next();
+}
