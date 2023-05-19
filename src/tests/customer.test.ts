e11261ed22689ea023f85250b99fe7d06e6bb1b3
madson <madsonsilva2005@gmail.com>
2023-05-19 16:39:39 -0300

    adicionado testes de get de dados (com e sem autenticação)

--- src/tests/customer.test.ts
+++ src/tests/customer.test.ts
@@ -3,14 +3,18 @@ import { test, expect } from "vitest";
 import app from "../server";
 // import router from '../routes/customer.route';
 
+let idCustomer : string = ""
+
 test("Cadastro de user", async () => {
   const payload = {
     name: "Madson",
-    email: `madson${Math.random()}@gmail.com`,
+    email: "exemplo@gmail.com",
     password: "thissenhaissenha",
   };
 
   const response = await request(app).post("/customers").send(payload);
+
+  idCustomer = response.body.id
 
   expect(response.body).toHaveProperty("id");
   expect(response.body.name).toBe(payload.name);
@@ -43,4 +47,58 @@ test("Cadastro de user com email inválido", async () => {
   expect(response.status).toBe(400);
   expect(response.body).toHaveProperty("error");
   expect(response.body.error).toBe("Email inválido");
+});
+
+test("Cadastro de user com senha inválida", async () => {
+  const payload = {
+    name: "Madson",
+    email: "email@gmail.com",
+    password: "this",
+  };
+  const response = await request(app).post("/customers").send(payload);
+  expect(response.status).toBe(400);
+  expect(response.body).toHaveProperty("error");
+  expect(response.body.error).toBe(
+    "O campo password deve ter no minimo 6 caracteres."
+  );
+});
+
+test("Cadastro de user com nome inválido", async () => {
+  const payload = {
+    name: "Ma",
+    email: "exemplo@gmail.com",
+    password: "thissenhaissenha"
+  };
+  const response = await request(app).post("/customers").send(payload);
+  expect(response.status).toBe(400);
+  expect(response.body).toHaveProperty("error");
+  expect(response.body.error).toBe(
+    "O campo name deve ter no minimo 3 caracteres"
+  );
+});
+
+test("GET de dados do usuário não autenticado", async () => {
+  const response = await request(app).get(`/customers/${idCustomer}`)
+  expect(response.status).toBe(400)
+  expect(response.body).toHaveProperty("error")
+  expect(response.body.error).toBe("Não autorizado")
+})
+
+test("GET de dados do usuário autenticado", async () => {
+  const payload = {
+    email: "exemplo@gmail.com",
+    password: "thissenhaissenha",
+  };
+
+  const responseLogin = await request(app).post("/customers/login").send(payload);
+  const id = responseLogin.body.id; 
+  const token = responseLogin.body.token;
+  const response = await request(app)
+    .get(`/customers/${id}`)
+    .set("Authorization", `Bearer ${token}`);
+  expect(response.status).toBe(200);
+  expect(response.body).toHaveProperty("id");
+  expect(response.body).toHaveProperty("name");
+  expect(response.body).toHaveProperty("email");
+  expect(response.body).not.toHaveProperty("password");
 });
